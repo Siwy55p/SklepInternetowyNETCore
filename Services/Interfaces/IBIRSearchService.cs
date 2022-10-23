f8f22bd764b4c6f213eea568102c8aa9d6574b32
Siwy55p <szuminski.p@gmail.com>
2022-10-23 20:47:05 +0200

    zalaczenie Service do projektu BIRServis

---
+++ Services/Interfaces/IBIRSearchService.cs
@@ -0,0 +1,21 @@
+﻿using partner_aluro.Models;
+
+namespace partner_aluro
+{
+    public interface IBIRSearchService
+    {
+        /// <summary>
+        /// Zwraca dane podmiotu w postaci modelu na postawie numeru identyfikacji VAT.
+        /// </summary>
+        /// <param name="vatId">Nr indentyfikacji podatkowej VAT</param>
+        /// <returns></returns>
+        Task<DanePodmiotu> GetCompanyDataByNipIdAsync(string vatId);
+        /// <summary>
+        /// Zwraca dane podmiotu w postaci modelu na podstawie numeru Regon.
+        /// </summary>
+        /// <param name="regonId">Nr identyfikacyjny Regon</param>
+        /// <returns></returns>
+        Task<DanePodmiotu> GetCompanyDataByRegonAsync(string regonId);
+
+    }
+}
