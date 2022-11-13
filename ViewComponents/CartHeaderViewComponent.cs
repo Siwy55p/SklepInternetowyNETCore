a5d26eb87939b8e58403d55cddf610de4fb2ea1d
Siwy55p <szuminski.p@gmail.com>
2022-11-13 17:39:28 +0100

    poprawki

--- ViewComponents/CartHeaderViewComponent.cs
+++ ViewComponents/CartHeaderViewComponent.cs
@@ -11,20 +11,11 @@ namespace partner_aluro.ViewComponents
     {
         private readonly ApplicationDbContext _context;
         private readonly Cart _cart;
-        private readonly IProfildzialalnosciService _profildzialalnosciService;
-
-        private readonly IHttpContextAccessor _httpContextAccessor;
-
-        private readonly UserManager<ApplicationUser> _userManager;
 
         public CartHeaderViewComponent(Cart cart, ApplicationDbContext applicationDbContext, UserManager<ApplicationUser> userManager, IHttpContextAccessor httpContextAccessor, IProfildzialalnosciService profildzialalnosciService)
         {
             _cart = cart;
             _context = applicationDbContext;
-
-            _userManager = userManager;
-            _httpContextAccessor = httpContextAccessor;
-            _profildzialalnosciService = profildzialalnosciService;
         }
         public async Task<IViewComponentResult> InvokeAsync()
         {
