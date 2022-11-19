56ef6b687448f24cfdef88efe967f9d38411bf20
Siwy55p <szuminski.p@gmail.com>
2022-11-20 00:33:45 +0100

    tiny

--- wwwroot/js/tinymce.js
+++ wwwroot/js/tinymce.js
@@ -57,8 +57,9 @@ tinymce.init({
     selector: 'textarea',
     language: 'pl',
     language_url: 'https://partneralluro.hostingasp.pl/js/tinymce/langs/pl.js',  // site absolute URL
-    relative_urls: true,
-    document_base_url: 'https://partneralluro.hostingasp.pl/',
+    relative_urls: false,
+    remove_script_host: false,
+    convert_urls: true,
     plugins: 'anchor autolink charmap codesample emoticons image link lists media searchreplace table visualblocks wordcount checklist mediaembed casechange export formatpainter pageembed linkchecker a11ychecker tinymcespellchecker permanentpen powerpaste advtable advcode editimage tinycomments tableofcontents footnotes mergetags autocorrect',
     toolbar: 'undo redo | blocks fontfamily fontsize | bold italic underline strikethrough | link image media table mergetags | addcomment showcomments | spellcheckdialog a11ycheck | align lineheight | checklist numlist bullist indent outdent | emoticons charmap | removeformat',
     tinycomments_mode: 'embedded',
